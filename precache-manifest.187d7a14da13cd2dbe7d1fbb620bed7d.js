09f5b01cc08ba75034d76e6f21acce5b6816c68a
petrlr14 <pedrodeveloper14@uca.edu.sv>
2019-10-31 15:34:40 -0600

    Updates

---
+++ precache-manifest.187d7a14da13cd2dbe7d1fbb620bed7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d40ba5a69403bf03981d720c6601128",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "a9f7b104693f466c189f",
+    "url": "/tictactoe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "/tictactoe/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "a9f7b104693f466c189f",
+    "url": "/tictactoe/static/js/main.46997a5d.chunk.js"
+  },
+  {
+    "revision": "1dd1878a39f419eb6036",
+    "url": "/tictactoe/static/js/runtime-main.868a150c.js"
+  }
+]);
